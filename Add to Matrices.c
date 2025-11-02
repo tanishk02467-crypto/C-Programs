eeade32d76e8bb848fa275128053292f5c3ebc6a
Tanishk Saraswat <tanishksaraswat02467@gmail.com>
2025-11-02 12:00:12 +0530

    Add files via upload

---
+++ Add to Matrices.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main() {
+    int rows, cols, i, j;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+    printf("Enter number of columns: ");
+    scanf("%d", &cols);
+
+    int A[rows][cols], B[rows][cols], sum[rows][cols];
+
+    printf("Enter elements of Matrix A:\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            scanf("%d", &A[i][j]);
+        }
+    }
+    printf("Enter elements of Matrix B:\n");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            scanf("%d", &B[i][j]);
+        }
+    }
+
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            sum[i][j] = A[i][j] + B[i][j];
+        }
+    }
+
+    printf("Sum of the matrices:");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            printf("%d\t", sum[i][j]);
+        }
+    
+    }
+
+}
---
+++ the sum of all elements in a matrix.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int rows, cols, i, j, sum = 0;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+    printf("Enter number of columns: ");
+    scanf("%d", &cols);
+
+    int matrix[rows][cols];
+
+    printf("Enter elements of the matrix: ");
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+            sum += matrix[i][j]; 
+        }
+    }
+
+    printf("Sum of all elements in the matrix = %d\n", sum);
+
+}
